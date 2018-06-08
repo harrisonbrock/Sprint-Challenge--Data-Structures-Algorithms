442f697a429e1a83a139fcee78bd44f90cf2a9b6
harrisonbrock <harrison@harrisonbrock.com>
2018-06-08 11:23:09 -0400

    Heapsort is done

--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,68 +1,79 @@
 const heapsort = (arr) => {
-  /* Your code here */
-  
+
+    const heap = new Heap();
+    // insert items into new heap
+    arr.forEach(item => heap.insert(item));
+
+    const arrCopy = [];
+
+    while (heap.getSize()) {
+        arrCopy.unshift(heap.delete());
+    }
+
+    return arrCopy
+
 };
 
 
 class Heap {
-  constructor() {
-    this.storage = [];
-  }
-
-  insert(val) {
-    const index = this.storage.push(val) - 1;
-    this.bubbleUp(index);
-  }
-
-  delete() {
-    if (!this.storage.length) return null;
-    if (this.storage.length === 1) {
-      return this.storage.pop();
+    constructor() {
+        this.storage = [];
     }
 
-    const max = this.storage[0];
-    this.storage[0] = this.storage.pop();
-    this.siftDown(0);
-    return max;
-  }
-
-  getMax() {
-    return this.storage[0];
-  }
-
-  getSize() {
-    return this.storage.length;
-  }
-
-  bubbleUp(index) {
-    const parent = Math.floor((index - 1) / 2);
-    if (this.storage[parent] < this.storage[index]) {
-      [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
-      this.bubbleUp(parent);
+    insert(val) {
+        const index = this.storage.push(val) - 1;
+        this.bubbleUp(index);
     }
-  }
 
-  siftDown(index) {
-    const leftChildIndex = index * 2 + 1;
-    const rightChildIndex = index * 2 + 2;
-    let maxChild;
-    
-    if (this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
-      maxChild = this.storage[leftChildIndex] > this.storage[rightChildIndex] ? leftChildIndex : rightChildIndex;
-    } else if (this.storage[leftChildIndex]) {
-      maxChild = leftChildIndex;
-    } else if (this.storage[rightChildIndex]) {
-      maxChild = rightChildIndex;
-    } 
+    delete() {
+        if (!this.storage.length) return null;
+        if (this.storage.length === 1) {
+            return this.storage.pop();
+        }
 
-    if (this.storage[index] < this.storage[maxChild]) {
-      [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
-      this.siftDown(maxChild);
+        const max = this.storage[0];
+        this.storage[0] = this.storage.pop();
+        this.siftDown(0);
+        return max;
     }
-  }
+
+    getMax() {
+        return this.storage[0];
+    }
+
+    getSize() {
+        return this.storage.length;
+    }
+
+    bubbleUp(index) {
+        const parent = Math.floor((index - 1) / 2);
+        if (this.storage[parent] < this.storage[index]) {
+            [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
+            this.bubbleUp(parent);
+        }
+    }
+
+    siftDown(index) {
+        const leftChildIndex = index * 2 + 1;
+        const rightChildIndex = index * 2 + 2;
+        let maxChild;
+
+        if (this.storage[leftChildIndex] && this.storage[rightChildIndex]) {
+            maxChild = this.storage[leftChildIndex] > this.storage[rightChildIndex] ? leftChildIndex : rightChildIndex;
+        } else if (this.storage[leftChildIndex]) {
+            maxChild = leftChildIndex;
+        } else if (this.storage[rightChildIndex]) {
+            maxChild = rightChildIndex;
+        }
+
+        if (this.storage[index] < this.storage[maxChild]) {
+            [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
+            this.siftDown(maxChild);
+        }
+    }
 }
 
 module.exports = {
-  Heap,
-  heapsort,
+    Heap,
+    heapsort,
 };
